e2cf2285cc65179d2a7ca5ab7878a39266d782c0
crchrin <crchrin@gmail.com>
2022-01-24 22:11:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
+
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+  createCanvas(600, 600);
 }
 
 function draw() {
-	background(225);
-	textSize(32);
-	fill(180);
-	text(hour(), 10, 30);
-	fill(100);
-	text(minute(), 10, 60);
-	fill(0);
-	text(second(), 10, 90);
+  background(214, 80, 118);
+  let s = second();
+  let m = minute();
+  let h = hour();
+  console.log(m);
+  strokeWeight(1);
+  line(0,(s*10),600,(s*10));
+  stroke('white');
+  strokeWeight(2);
+  line(0,(m*10),600,(m*10));
+  strokeWeight(3);
+  line(0,(h*25),600,(h*25));
 }
